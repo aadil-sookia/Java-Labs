1e9378ea1479ef08dff3003208b823eb8453785a
aadil-sookia <aadil.sookia@gmail.com>
2015-01-11 17:14:34 -0500

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,32 @@
+//import java.io.*;
+
+public class Node {
+    
+    public static Pro3_sookiamu M = new Pro3_sookiamu ();
+    public static Graph G = new Graph ();
+    private String name;
+    private double latitude;
+    private double longitude;
+   
+    //constructors
+    public Node () {this.name = "null"; this.latitude = 0.; this.longitude = 0.;}
+    public Node (String city_name, double lat, double longi) {
+        this.name = city_name;
+        this.latitude = lat;
+        this.longitude = longi;
+    }
+    
+    //setters
+    public void setName (String city_name) { this.name = city_name; }
+    public void setLatitude (double lat) { this.latitude = lat; }
+    public void setLongitude (double longi) { this.longitude = longi; }
+       
+    //getters
+    public String getName () { return this.name; }
+    public double getLatitude () { return this.latitude; }
+    public double getLongitude () { return this.longitude; }
+   
+        
+}
+
+
